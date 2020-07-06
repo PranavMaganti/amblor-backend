33976b93ec2a5b84849054329649a5f12f662ddb
Pranav Maganti <pranav.maganti@gmail.com>
2020-07-06 21:37:50 +0100

    Remove cached files

--- modules/util.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-
-
-def none_or_str(text: str) -> Optional[str]:
-    """ Returns None if string is empty and the string otherwise """
-    if text == "":
-        return None
-
-    return text
